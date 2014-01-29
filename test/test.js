4bd783329989e829b56f85ee9c61764d109b1e13
John Josef <john.josef@gmail.com>
2014-01-29 08:57:46 -0500

    bump version

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ var shopifyObj = require('../lib/shopify');
 
 var shopify = new shopifyObj({
   shop_name: 'typefoo',
-  id: '639e5b59d03a4135d4f4cd176d8b0d0c',
-  secret: '07e3e4d5711054ead625ac7356552660',
+  id: '',
+  secret: '',
   redirect: 'http://localhost:9000/#/oauth'
 });
 
